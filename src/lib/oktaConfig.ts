803023f5ee91a9b0b24c012a13c9a7c4d4e82fd1
Mateusz Baryla <m.baryla@protonmail.ch>
2023-09-04 09:00:11 +0200

    Security and Authentication implemented using Okta

---
+++ src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+  clientId: "0oab35fbv8tL1b4ot5d7",
+  issuer: "https://dev-05776184.okta.com/oauth2/default",
+  redirectUri: "http://localhost:3000/login/callback",
+  scopes: ["openid", "profile", "email"],
+  pkce: true,
+  disableHttpsCheck: true,
+};
